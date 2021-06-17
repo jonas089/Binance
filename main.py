a1219634c618ae6dd3f262162d04139ee5e33f40
Jonas Pauli <jonaspauli089@gmail.com>
2021-06-17 20:30:39 +0200

    Enter Long & Short

--- main.py
+++ main.py
@@ -3,19 +3,29 @@ from flask import Flask, request
 import time
 import json
 import pickle
-
-leverages = {'ADA':1, 'DOT':2, 'ETH':4, 'LTC':1, 'XMR':7}
+# Create Files
 try:
     open('history.dat', 'x')
     open('first.dat', 'x')
+    open('positions.dat', 'x')
+    open('leverages.dat', 'x')
 except Exception as exists:
-    pass
+    print('Warning! Files exist.')
+
+# Instantiate
 with open('first.dat', 'wb') as first:
     initial_data = {'ADA':True, 'DOT':True, 'ETH':True, 'LTC':True, 'XMR':True}
     pickle.dump(initial_data, first)
 with open('history.dat', 'wb') as history:
     initial_data = {'ADA':0, 'DOT':0, 'ETH':0, 'LTC':0, 'XMR':0}
     pickle.dump(initial_data, history)
+with open('positions.dat', 'wb') as pos:
+    positions = {'ADA':'None', 'DOT':'None', 'ETH':'None', 'XMR':'None'}
+    pickle.dump(positions, pos)
+with open('leverages.dat', 'wb') as lev:
+    leverages = {'ADA':1, 'DOT':2, 'ETH':4, 'LTC':1, 'XMR':7}
+    pickle.dump(leverages, lev)
+
 api_key = 'w5WslwajZVtl45kJdSsU6aTDW55ZmMyn9vy7txcJnGTxmBzs92MV7hTnMCYDTyVE'
 secret_key = 'sYOZrlfkgcRpteYXUhYSvEmrngpwCu6TIdxhKYTXqMXzXJEQ1NCiFYW1AwD1MUvv'
 app = Flask(__name__)
@@ -57,7 +67,13 @@ def Action():
             with open('first.dat', 'wb') as first:
                 pickle.dump(is_first, first)
             return 0
+    with open('leverages.dat', 'rb') as lev:
+        leverages = pickle.load(lev)
+        leverage = float(leverages[ticker])
 
+    with open('positions.dat', 'rb') as pos:
+        positions = pickle.load(pos)
+        position = positions[ticker]
 
     if str(data) == 'buy':
         with open('history.dat', 'rb') as history:
@@ -70,12 +86,40 @@ def Action():
             is_first[ticker] = False
         with open('first.dat', 'wb') as first:
             pickle.dump(is_first, first)
-        return client.futures_create_order(symbol=(ticker+'USDT'), side='BUY', type='MARKET', quantity=(float(amount) * leverages[ticker]))
+
+        if position == 'None':
+            with open('positions.dat', 'wb') as pos:
+                positions[ticker] = 'Buy'
+                pickle.dump(positions, pos)
+        elif position == 'Sell':
+            with open('positions.dat', 'wb') as pos:
+                positions[ticker] = 'None'
+                # Close old position and open new Long position,
+                client.futures_create_order(symbol=(ticker+'USDT'), side='BUY', type='MARKET', quantity=(float(amount) * leverage))
+                positions[ticker] = 'Buy'
+                # comment above out, to only entry long / short. Keep active, to enter short & long.
+                pickle.dump(positions, pos)
+
+        return client.futures_create_order(symbol=(ticker+'USDT'), side='BUY', type='MARKET', quantity=(float(amount) * leverage))
 
     elif str(data) == 'sell':
         with open('history.dat', 'rb') as history:
             positioned_amount = pickle.load(history)[ticker]
-        return client.futures_create_order(symbol=(ticker+'USDT'), side='SELL', type='MARKET', quantity=(positioned_amount * leverages[ticker]))
+
+        if position == 'None':
+            with open('positions.dat', 'wb') as pos:
+                positions[ticker] = 'Sell'
+                pickle.dump(positions, pos)
+        elif position == 'Buy':
+            with open('positions.dat', 'wb') as pos:
+                positions[ticker] = 'None'
+                # Close old position and open new Long position,
+                client.futures_create_order(symbol=(ticker+'USDT'), side='SELL', type='MARKET', quantity=(positioned_amount * leverage))
+                positions[ticker] = 'Sell'
+                # comment above out, to only entry long / short. Keep active, to enter short & long.
+                pickle.dump(positions, pos)
+
+        return client.futures_create_order(symbol=(ticker+'USDT'), side='SELL', type='MARKET', quantity=(positioned_amount * leverage))
     else:
         print('Warning: ' + str(data))
     return 0
